b282c09cbcc83ee9a950090674f17e3c30ff1d8b
Nikolay Khokhlov <hohlovkola@gmail.com>
2020-03-12 16:44:56 +0300

    added

---
+++ lab.c
@@ -0,0 +1,183 @@
+#include "nvector.h"
+
+Vector* vscan(type tp) {
+
+	Vector* vptr;
+
+	printf("\nenter the size of vector\n");
+
+	int size;
+	scanf("%d",&size);
+  
+    if (tp == INT) {
+    	vptr = vcreate(INT,size);
+    	printf("\nenter %d integer elements\n", size);
+    	
+    	int temp;
+    	for (int i = 0; i < size; ++i) {
+    		scanf("%d",&temp);
+    		vset_elem(vptr,i,&temp);
+    	}
+    }
+
+    if (tp == COMPLEX){
+    	vptr = vcreate(COMPLEX,size);
+    	printf("\nenter %d complex elements", size);
+
+    	double img,real;
+
+    	for (int i = 0; i < size; ++i) {
+    		printf("\nreal part");
+    		scanf("%le",&real);
+    		printf("image part");
+    		scanf("%le",&img);
+    		complex z = CMPLX(real,img);
+    		vset_elem(vptr,i,&z);
+    	}
+    }
+ 	
+ 	return vptr;
+}
+
+void ui () {
+	int choice;
+
+	Vector* varr[6] = {NULL} ;
+
+	void clean() {
+
+		for (int i = 0; i < 6; ++i) {
+			if (varr[i] != NULL) {
+			vfree(varr[i]);
+			}
+		}
+	}
+
+	while(1){
+		printf("%s\n",
+			"1. set int vector1\n"
+			"2. set int vector2\n"
+			"3. set compl vector3\n"
+			"4. set compl vector4\n"
+			"5. get vector1 + vector2\n"
+			"6. get vector3 + vector4\n"
+			"7. get scalar multiply of int vectors\n"
+			"8. get scalar multiply of compl vectors\n"
+			"9. get elem\n"
+			"10. delete choosen vector\n"
+			"11. exit\n"
+		);
+
+		int iscr,cscr;
+		scanf("%d", &choice); 
+		
+		switch(choice) {
+			case 1:
+				varr[0] = vscan(INT);
+				printf("vector1 = ");
+				vprint(varr[0]);
+				break;
+			case 2:
+				varr[1] = vscan(INT);
+				printf("vector2 = ");
+				vprint(varr[1]);
+				break;
+			case 3:
+				varr[2] = vscan(COMPLEX);
+				printf("vector3 = ");
+				vprint(varr[2]);
+				break;
+			case 4:
+				varr[3] = vscan(COMPLEX);
+				printf("vector4 = ");
+				vprint(varr[3]);
+				break;				
+			case 5:
+				if ((varr[0] == NULL) || (varr[1] == NULL)) {
+					clean();
+					printf("error: null vector\n");
+					exit(1);
+				}
+				varr[4] = vadd(varr[0], varr[1]);
+				printf("vector5 = ");
+				vprint(varr[4]);
+				break;
+			case 6:
+				if ((varr[2] == NULL) || (varr[3] == NULL)) {
+					clean();
+					printf("error: null vector\n");
+					exit(1);
+				}
+				varr[5] = vadd(varr[2], varr[3]);
+				printf("vector6 = ");
+				vprint(varr[5]);
+				break;
+			case 7:
+				if ((varr[0] == NULL) || (varr[1] == NULL)) {
+					clean();
+					printf("error: null vector\n");
+					exit(1);
+				}
+				iscr = *(int*)vscal_mult(varr[0], varr[1]);
+				printf("%d\n",iscr);
+				break;
+			case 8:
+				if ((varr[0] == NULL) || (varr[1] == NULL)) {
+					clean();
+					printf("error: null vector\n");
+					exit(1);
+				}
+				cscr = *(complex*)vscal_mult(varr[2], varr[3]);
+				printf("%d\n",cscr);
+				break;
+			case 9:
+				printf("enter the number of vector\n");
+				int num1;
+				scanf("%d",&num1);
+
+				if ((num1 < 1) && (num1 > 6)) {
+					exit(1);
+				}
+
+				if (varr[num1-1] == NULL) {
+					clean();
+					printf("error: null vector\n");
+					exit(1);
+				}
+
+				int ind;
+				printf("enter index\n");
+				scanf("%d",&ind);
+
+				if (varr[num1-1]->str_type == INT) {
+					printf("%d\n",*(int*)vget_elem(varr[num1-1],ind));
+				} else {
+					complex tmp = *(complex*)vget_elem(varr[num1-1],ind);
+					printf("%g %+gi\n",creal(tmp),cimag(tmp));
+				}
+				break;	
+			case 10:
+				printf("enter the number of vector to delete");
+				int num;
+				scanf("%d",&num);
+
+				if ((num >= 1) && (num <= 6) && (varr[num-1] != NULL)) {
+					vfree(varr[num-1]);
+				} else {
+					clean();
+					printf("error: null vector\n");
+					exit(1);
+				}
+				break;	
+			case 11:
+				return;
+			default:
+				exit(1);
+				break;		
+		}
+	}
+}
+
+int main() {
+	ui();
+}
---
+++ nvector.h
@@ -0,0 +1,192 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+#include <stdlib.h>
+#include <complex.h>
+
+typedef enum {INT = 1, COMPLEX} type;	
+
+typedef struct {
+	void* buffer;
+	int count;
+	type str_type;
+} Vector;
+
+Vector* vcreate(type str_type,int count) {
+    
+    if (count <= 0 ) {
+    	printf("error: wrong size\n");
+    	exit(1);
+    }
+
+    if ((str_type < 1) || (str_type > 2)) {
+    	printf("error: wrong type\n");
+    	exit(1);
+    }
+
+    Vector* vptr = (Vector*)malloc(sizeof(Vector));
+    
+    int elem_size;
+    switch(str_type) {
+    	case INT:
+    		elem_size = sizeof(int);
+    		break;
+    	case COMPLEX:
+    		elem_size = sizeof(complex);
+    		break;
+    	default : 
+    		break;
+    }
+
+    vptr->str_type = str_type;
+    vptr->count = count;
+    vptr->buffer = malloc(count*elem_size);
+
+    return vptr;
+}
+
+void* vget_elem(Vector* vec,int index) {
+	
+	if ((index < 0) || (index > vec->count)) {
+		printf("error: index out of range\n");
+		exit(1);
+	}
+
+	if (vec == NULL) {
+		printf("error: null vector\n");
+		exit(1);
+	}
+
+    switch(vec->str_type) {
+    	case INT:
+    		return ((int*)(vec->buffer)+index);
+    		break;
+    	case COMPLEX:
+    		return ((complex*)(vec->buffer)+index);
+    		break;
+    	default : 
+    		break;
+    }
+} 
+
+void vset_elem(Vector* vec, int index, void* value) {
+    
+    if (vec == NULL) {
+		printf("error: null vector\n");
+		exit(1);
+	}
+
+    int elem_size;
+    
+    switch(vec->str_type) {
+    	case INT:
+    		elem_size = sizeof(int);
+    		break;
+    	case COMPLEX:
+    		elem_size = sizeof(complex);
+    		break;
+    	default : 
+    		break;
+    }
+
+    void *p = (char*)vec->buffer + index*elem_size;
+    memcpy(p, value, elem_size);
+}
+
+Vector* vadd(Vector* vec1, Vector* vec2) {
+	
+	if ((vec1 == NULL) || (vec2 ==NULL)) {
+		printf("error: null vector\n");
+		exit(1);
+	}	
+
+	if ((vec1->str_type != vec2->str_type) || (vec1->count != vec2->count)) {
+		printf("error: wrong types format\n");
+		exit(1);
+	}
+
+	Vector* vptr = vcreate(vec1->str_type, vec1->count);
+	
+	for (int i = 0; i < vec1->count; ++i) {
+		if (vec1->str_type == INT) {
+			((int*)vptr->buffer)[i] = ((int*)vec1->buffer)[i] + ((int*)vec2->buffer)[i];
+		} else if (vec1->str_type == COMPLEX) {
+			((complex*)vptr->buffer)[i] = ((complex*)vec1->buffer)[i] + ((complex*)vec2->buffer)[i];						
+		}
+	}
+
+	return vptr;
+}
+
+void* vscal_mult(Vector* vec1, Vector* vec2) {
+	
+    if ((vec1 == NULL) || (vec2 ==NULL)) {
+		printf("error: null vector\n");
+		exit(1);
+	}	
+
+    switch(vec1->str_type) {
+    	case INT:
+			
+			if ((vec1->count != vec2->count) || (vec1->str_type != INT) || (vec2->str_type != INT)) {
+				printf("error: wrong types format\n");
+				exit(1);
+			} 
+
+			int* ires = (int*)malloc(sizeof(int));
+			*ires = 0;
+				
+			for (int i = 0; i < vec1->count; ++i) {
+				*ires += ((int*)vec1->buffer)[i] * ((int*)vec2->buffer)[i];
+			} 
+
+			return(ires);
+    		break;
+    	case COMPLEX:
+			
+			if ((vec1->count != vec2->count) || (vec1->str_type != COMPLEX) || (vec2->str_type != COMPLEX)) {
+				printf("error: wrong types format\n");
+				exit(1);
+			} 
+
+			complex* cres = (complex*)malloc(sizeof(complex));;
+					
+			for (int i = 0; i < vec1->count; ++i) {
+				*cres += ((complex*)vec1->buffer)[i] * conj(((complex*)vec2->buffer)[i]);						
+			}
+				
+			return cres;	    		
+  	 		break;
+    	default : 
+    		break;
+    }
+} 
+
+void vfree(Vector* vec) {
+	
+	if (vec == NULL) {
+		printf("error: null vector\n");
+		exit(1);
+	}
+
+	free(vec->buffer);
+	free(vec);
+}
+
+void vprint(Vector* vec) {
+	
+	if (vec == NULL) {
+		printf("error: null vector\n");
+		exit(1);
+	}
+
+	for (int i = 0; i < vec->count ; ++i) {
+		if (vec->str_type == COMPLEX) { 
+			printf("%g %+gi ",creal(((complex*)vec->buffer)[i]),cimag(((complex*)vec->buffer)[i]));
+		} else {	 
+			printf("%d ",((int*)vec->buffer)[i]);
+		}	
+	};
+
+	printf("\n");
+}
